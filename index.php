2f0ebb93b86acb9deb0e201f57ca29699574bc3e
sanjee <79707868+sanjeevan1@users.noreply.github.com>
2023-09-23 14:36:45 +0530

    Update index.php
    https://github.com/sanjeevan1/bash-

--- index.php
+++ index.php
@@ -1,18 +1,2 @@
 
-        <textarea name="note" rows="4" cols="50"></textarea><br><br>
-        
-        <label for="address">Delivery Address:</label>
-        <input type="text" name="address"><br><br>
-        
-        <label for="delivery_time">Delivery Time:</label>
-        <select name="delivery_time">
-            <option value="09:00-11:00">09:00 - 11:00</option>
-            <option value="11:00-13:00">11:00 - 13:00</option>
-            <option value="13:00-15:00">13:00 - 15:00</option>
-            <!-- Add more time slot options as needed -->
-        </select><br><br>
-        
-        <input type="submit" value="Place Order">
-    </form>
-</body>
-</html>
+        <tml>
